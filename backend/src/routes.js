e6683dd68e3ae1fd8136840a537e6bfa242a0fc8
thiagoc-machado <thiagocmachado@yahoo.com.br>
2023-02-08 21:30:00 +0100

    change priority

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,9 +3,13 @@ const AnnotationController = require('./controllers/AnnotationController');
 const routes = express.Router();
 
 const AnnotetionControler = require('./controllers/AnnotationController');
+const PriorityController = require('./controllers/PriorityController');
 
 routes.post('/annotations', AnnotetionControler.create);
 routes.get('/annotations', AnnotationController.read);
 routes.delete('/annotations/:id', AnnotationController.delete);
+
+routes.get('/priorities', PriorityController.read);
+routes.post('/priorities/:id', PriorityController.update);
 
 module.exports = routes;
